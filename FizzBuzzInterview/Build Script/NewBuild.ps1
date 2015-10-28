088649040fab5c3dbb2c59fd038e287020285620
Matt Sonnhalter <matthew9559@gmail.com>
2015-10-28 17:00:05 -0600

    Added new psake build script and associated tasks

---
+++ FizzBuzzInterview/Build Script/NewBuild.ps1
@@ -0,0 +1,6 @@
+﻿Set-ExecutionPolicy RemoteSigned
+Import-Module psake;
+
+$buildTask = "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview\Build Script\NewBuildTasks.ps1"
+
+Invoke-psake $buildTask
---
+++ FizzBuzzInterview/Build Script/NewBuildTasks.ps1
@@ -0,0 +1,16 @@
+﻿#psake build script tasks for FizzBuzzInterview solution
+
+task -name Clean -description "Deletes all build artifacts" -action {
+    exec { 
+            msbuild "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview.sln" /t:Clean
+        }
+}
+
+task -name Build -description "Builds the outdated artifacts" -action { 
+    exec { 
+            msbuild "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview.sln" /t:Build
+        }
+}
+
+
+task -name Default -depends Clean, Build -description "The default action will be to rebuild all artifacts"
