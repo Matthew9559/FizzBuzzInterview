9de533c20906842734aeb60c63304036502093d4
Matt Sonnhalter <matthew9559@gmail.com>
2015-10-28 17:12:24 -0600

    Fixed Build script

--- FizzBuzzInterview/Build Scripts/NewBuild.ps1
+++ FizzBuzzInterview/Build Scripts/NewBuild.ps1
@@ -1,6 +1,6 @@
 ﻿Set-ExecutionPolicy RemoteSigned
 Import-Module psake;
 
-$buildTask = "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview\Build Script\NewBuildTasks.ps1"
+$buildTask = "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview\Build Scripts\NewBuildTasks.ps1"
 
 Invoke-psake $buildTask
