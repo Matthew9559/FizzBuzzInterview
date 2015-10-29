3e48768a7a6bb4c706148bbd086c197f82eedf9e
Matt Sonnhalter <matthew9559@gmail.com>
2015-10-29 03:16:16 -0600

    Added property to the build command so that user can select whether to do a Dev or Production release

--- FizzBuzzInterview/Build Scripts/NewBuild.ps1
+++ FizzBuzzInterview/Build Scripts/NewBuild.ps1
@@ -3,4 +3,5 @@ Import-Module psake;
 
 $buildTask = "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview\Build Scripts\NewBuildTasks.ps1"
 
-Invoke-psake $buildTask
+#Change config value in the properrty below to values 'debug' or 'release' depending on build desired
+Invoke-psake $buildTask -properties @{ 'config'='Debug'}
--- FizzBuzzInterview/Build Scripts/NewBuildTasks.ps1
+++ FizzBuzzInterview/Build Scripts/NewBuildTasks.ps1
@@ -1,16 +1,19 @@
 ﻿#psake build script tasks for FizzBuzzInterview solution
 
+properties {
+    $config = 'Debug'; 
+}
+
 task -name Clean -description "Deletes all build artifacts" -action {
     exec { 
-            msbuild "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview.sln" /t:Clean
+            msbuild "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview.sln" /t:Clean /p:Configuration=$config
         }
 }
 
 task -name Build -description "Builds the outdated artifacts" -action { 
     exec { 
-            msbuild "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview.sln" /t:Build
+            msbuild "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview.sln" /t:Build /p:Configuration=$config
         }
 }
 
-
-task -name Default -depends Clean, Build -description "The default action will be to rebuild all artifacts"
+task -name Default -depends Clean, Build -description "The default action will be to rebuild all artifacts and deploy a build"
