43d34c66fc987ebc0a66df5626d51c177829c242
Matt Sonnhalter <matthew9559@gmail.com>
2015-10-29 03:42:15 -0600

    Added validation for NUnit dll check

--- FizzBuzzInterview/Build Scripts/NewBuildTasks.ps1
+++ FizzBuzzInterview/Build Scripts/NewBuildTasks.ps1
@@ -4,17 +4,21 @@ properties {
     $config = 'Debug'; 
 }
 
+task -name ValidateNUnit -action {
+    assert -conditionToCheck (Test-Path("\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\packages\NUnit.2.6.4\lib\nunit.framework.dll")) -failureMessage "NUnit must be installed in order to run tests"
+    }
+
 task -name ValidateConfig -action { 
     assert -conditionToCheck ('Debug','Release' -contains $config ) -failureMessage "Please select Debug or Release only" 
     }
 
-task -name Clean -depends ValidateConfig -description "Deletes all build artifacts" -action {
+task -name Clean -depends ValidateConfig, ValidateNUnit -description "Deletes all build artifacts" -action {
     exec { 
             msbuild "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview.sln" /t:Clean /p:Configuration=$config
         }
 }
 
-task -name Build -depends ValidateConfig -description "Builds the outdated artifacts" -action { 
+task -name Build -depends ValidateConfig, ValidateNUnit -description "Builds the outdated artifacts" -action { 
     exec { 
             msbuild "\\psf\Dropbox\Software Projects\C#\FizzBuzzInterview\FizzBuzzInterview.sln" /t:Build /p:Configuration=$config
         }
